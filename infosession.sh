9f84c4c037ae75382a1f3f56896c72403d4e02e4
marescalo <alu0101567211@ull.edu.es>
2024-10-30 11:36:30 +0000

    ssoo

--- infosession.sh
+++ infosession.sh
@@ -1,29 +1,42 @@
 #!/bin/bash
 
 usage(){
-    echo "usage: infosession.sh [-u user] [-z] [-h]"
+    echo "usage: infosession.sh [-h] [-z] [-u user]"
 }
 
-usuario=
+procesos="$(ps -e -o sid,pgid,pid,user,tty,%mem,cmd --no-headers --sort=user  | awk '{print $1, $2, $3, $4, $5, $6, $7}')"
+usuario=$USER
 opcion_z=0
+
+# si no se mete ninguna opcion
+
+if [ "$1" == "" ]; then
+
+  echo -e "$(ps -e -o sid,pgid,pid,user,tty,%mem,cmd --no-headers --sort=user | awk '{print $1,$2,$3,$4,$5,$6,$7}' | grep --invert-match 0.0)"
+
+fi
+
+# cuando se le meta una opcion
 
 while [ "$1" != "" ]; do
     case $1 in
-        -u | --user )
+        -u | -user)
             shift
-            usuario=$1
+            usuario=$2
             ;;
-        -z )
+        -z)
             opcion_z=1
             ;;
-        -h | --help ) 
+        -h | --help)
             usage
             exit
             ;;
-        * ) 
-            usage
-            exit 1
+        *) 
+            echo $procesos
+            exit 0
+            ;;
     esac
     shift
 done
+
 echo "muestra los procesos..."
